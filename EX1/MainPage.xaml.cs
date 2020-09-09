c67d992ad7213bf3db333b95f5ac93d37cf9f7c2
Your Name <huy hoang>
2020-09-09 13:25:02 +0700

    Assignment 1

---
+++ EX1/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using EX1.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EX1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            Email1 email1 = new Email1(Email.Text, Title1.Text, Content.Text);
+            LV.Items.Add(email1.toString());
+        }
+    }
+}
---
+++ EX1/Model/Email1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1.Model
+{
+    class Email1
+    {
+        private string email;
+        private string title;
+        private string content;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        public Email1(string email, string title, string content)
+        {
+            Email = email;
+            Title = title;
+            Content = content;
+        }
+        public string toString()
+        {
+            if (content.Length > 40)
+            {
+                string Str1 = content.Substring(0, 40);
+                return "Send to " + email + "\n" + "Title : " + title + "\n" + Str1 + "...";
+            }
+            else
+            {
+
+                return "-------------------------- \n" + "Send to " + email + "\n" + "Title : " + title + "\n" + content;
+            }
+        }
+    }
+}
